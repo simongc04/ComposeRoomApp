f5e6244061e17ca5c0190972a65b6f6bc3cc084f
simon <simonescuela9@gmail.com>
2024-11-15 10:29:33 +0000

    archivo task.kt creada

---
+++ app/src/main/java/com/example/composeroomapp/task.kt
@@ -0,0 +1,11 @@
+package com.example.composeroomapp
+
+
+import androidx.room.Entity import androidx.room.PrimaryKey
+
+@Entity(tableName = "tasks")
+data class Task
+    (@PrimaryKey(autoGenerate = true)
+                val id: Int = 0,
+                val name: String,
+                val isCompleted: Boolean = false )
